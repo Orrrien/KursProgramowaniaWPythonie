4160b289d1ea3f0db02dd392cf13611e843e1b33
Franciszek Stempień <57997325+Orrrien@users.noreply.github.com>
2024-02-19 01:39:52 +0100

    Add files via upload

---
+++ projekt_zaliczeniowy/data_analysis.py
@@ -0,0 +1,159 @@
+import json
+from geopy import distance
+from datetime import datetime
+import matplotlib
+
+
+class Stop:
+    def __init__(self, zespol, slupek, nazwa_zespolu,
+                 id_ulicy, szer_geo, dlug_geo, kierunek, obowiazuje_od):
+        self.zespol = zespol
+        self.slupek = slupek
+        self.nazwa_zespolu = nazwa_zespolu
+        self.id_ulicy = id_ulicy
+        self.szer_geo = szer_geo
+        self.dlug_geo = dlug_geo
+        self.kierunek = kierunek
+        self.obowiazuje_od = obowiazuje_od
+
+    def position(self):
+        return self.szer_geo, self.dlug_geo
+
+    def id(self):
+        return self.zespol, self.slupek
+
+
+# funkcja zliczająca liczbę autobusów w promieniu 0,5 km od punktu
+#   (w przypadku tej analizy: liczbę autobusów które przekroczyły prędkość 50km/h
+def speed_location(point_list, point):
+    if point not in point_list:
+        point_list.append([point, 1])
+    for i in range(len(point_list)):
+        if distance.distance(point_list[i][0], point) < 0.5 and point_list[i][0] != point:
+            point_list[i][1] += 1
+    return point_list
+
+
+# funkcja obliczająca w jakim czasie autobus przemieścił się między dwoma punktami
+def calculate_time(bus_list, index):
+    format = '%Y-%m-%d %H:%M:%S'
+    time1 = datetime.strptime(bus_list[index - 1].get('Time'), format)
+    time2 = datetime.strptime(bus_list[index].get('Time'), format)
+    time = time2 - time1
+    time = time.total_seconds() / 3600
+    return time
+
+
+# funkcja zwracająca pozycję autobusu jako krotkę
+def position(bus_list, index):
+    pos = (bus_list[index].get('Lat'), bus_list[index].get('Lon'))
+    return pos
+
+
+# funkcja obliczająca odległość między dwoma kolejnymi punktami (z response)
+def calculate_distance(bus_list, index):
+    pos1 = position(bus_list, index - 1)
+    pos2 = position(bus_list, index)
+    dis = distance.distance(pos1, pos2).km
+    return dis
+
+
+# funkcja zliczająca ile w sumie zostało zarejestrowanych autobusów podczas zbierania danych
+def number_of_buses(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return len(data)
+
+
+# funkcja znajdująca przystanek po danym id
+def find_stop(bus_stop_list, id):
+    for stop in bus_stop_list:
+        if stop.id() == id:
+            return stop
+    return None
+
+
+# funkcja zwracająca listę wszystkich przystanków (obiektów klasy Stop)
+def bus_stop_list(filename):
+    bus_stop_list = []
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    l = data.get('result')
+    for stop in l:
+        values = stop.get('values')
+        bus_stop = Stop(values[0].get('value'), values[1].get('value'), values[2].get('value'),
+                        values[3].get('value'), values[4].get('value'), values[5].get('value'),
+                        values[6].get('value'), values[7].get('value'))
+        bus_stop_list.append(bus_stop)
+    return bus_stop_list
+
+
+def bus_routes(filename, bus_stop_list):
+    bus_routes = ['dummy_line', ['dummy_stop']]
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    i = 1
+    dummy_stop = Stop('0', '0', '0', '0', '0', '0', '0', '0')
+    for line in data:
+        l = [line, [dummy_stop]]
+        bus_routes.append(l)
+        for route in data.get(line):
+            for stop in data.get(line).get(route):
+                bus_stop = find_stop(bus_stop_list, (data.get(line).get(route).get(stop).get('nr_zespolu'), data.get(line).get(route).get(stop).get('nr_przystanku')))
+                bus_routes[i][1].append(bus_stop)
+        i += 1
+    return bus_routes
+
+
+# funkcja wykonująca analizę prędkości autobusów:
+#   - przekroczenie 50km/h
+#   - nieprzekroczenie nigdy 30km/h
+def bus_speed_check(filename):
+    point_list = []
+    speed_counter = 0
+    slow_counter = 0
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    for bus_list in data:
+        speeding = False
+        slow = True
+        for j in range(1, len(bus_list)):
+            dis = calculate_distance(bus_list, j)
+            time = calculate_time(bus_list, j)
+            if time != 0:
+                vel = dis / time
+            else:
+                vel = 0
+            if vel > 30:
+                slow = False
+            if vel > 50:
+                speeding = True
+                point_list = speed_location(point_list, position(bus_list, j))
+        if speeding:
+            speed_counter += 1
+        if slow:
+            slow_counter += 1
+    return speed_counter, point_list, slow_counter
+
+
+filename = 'bus_positions_divided.json'
+filename2 = 'bus_positions_divided2.json'
+bus_stop_loc_filename = 'bus_stop_loc.json'
+bus_route_filename = 'bus_route_clean.json'
+
+tup1 = bus_speed_check(filename)
+tup2 = bus_speed_check(filename2)
+
+bus_stop_list = bus_stop_list(bus_stop_loc_filename)
+bus_routes = bus_routes(bus_route_filename, bus_stop_list)
+
+
+speeding1 = tup1[0]
+point_list1 = tup1[1]
+slow1 = tup1[2]
+all_buses1 = number_of_buses(filename)
+
+speeding2 = tup2[0]
+point_list2 = tup2[1]
+slow2 = tup2[2]
+all_buses2 = number_of_buses(filename2)
---
+++ projekt_zaliczeniowy/data_collection.py
@@ -0,0 +1,189 @@
+import time
+import requests
+import json
+from datetime import datetime, timedelta
+
+
+# funkcja pobierająca dane o loaklizacji autobusów
+def get_bus_location(url, api_key):
+    data = []
+    params = {
+        'resource_id': 'f2e5503e-927d-4ad3-9500-4ab9e55deb59',
+        'apikey': api_key,
+        'type': 1
+    }
+    for i in range(60):
+        response = requests.get(url, params)
+        data.append(response.json())
+        time.sleep(60)
+
+    return data
+
+
+# funkcja pobierająca dane o trasach autobusów
+def get_bus_route(url, api_key):
+    data = []
+    params = {
+        'apikey': api_key,
+    }
+
+    response = requests.get(url, params)
+    data.append(response.json())
+    return data
+
+
+def get_bus_stop_location(url, api_key):
+    data = []
+    params = {
+        #'id': 'ab75c33d-3a26-4342-b36a-6e5fef0a3ac3',
+        'apikey': api_key,
+    }
+
+    response = requests.get(url, params)
+    data.append(response.json())
+    return data
+
+
+# funkcja zapisaująca dane do pliku .json
+def save_to_json(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+# funkcja formatująca plik tak aby zawierał jedną listę słowników (response)
+def clean_file(filename, result_file):
+    bus_list = []
+    with open(filename, 'r') as file:
+        bus_data = json.load(file)
+        with open(result_file, 'w') as result_file:
+            for i in range(len(bus_data)):
+                for j in range(len(bus_data[i]["result"])):
+                    if type(bus_data[i]["result"]) != list:
+                        break
+                    bus_list.append(bus_data[i]["result"][j])
+            json.dump(bus_list, result_file, indent=4)
+
+    return result_file
+
+
+def clean_route_file(filename, result_file):
+    with open(filename, 'r') as file:
+        bus_data = json.load(file)
+        with open(result_file, 'w') as result_file:
+            bus_route_dict = bus_data[0]["result"]
+            json.dump(bus_route_dict, result_file, indent=4)
+
+    return result_file
+
+# funkcja formatująca plik tak aby zawierał listę L list l, gdzie lista l zawiera
+#   wszystkie słowniki (response) dotyczące jednego pojazdu, posortowane po czasie
+def divide_by_vehicle(result_file, sorted_filename, start_hour):
+    with open(result_file, 'r') as file:
+        vehicles = []
+        sorted_data = []
+        data = json.load(file)
+        with open(sorted_filename, 'w') as sorted_file:
+            for i in range(len(data)):
+                line_nr = data[i].get('Lines')
+                veh_id = data[i].get('VehicleNumber')
+                id = (line_nr, veh_id)
+                format = '%Y-%m-%d %H:%M:%S'
+                timestr = data[i].get('Time')
+                time = datetime.strptime(timestr, format)
+                if time.hour < start_hour:
+                    continue
+                if id not in vehicles:
+                    vehicles.append(id)
+                    list = [data[i]]
+                    sorted_data.append(list)
+                else:
+                    id = vehicles.index(id)
+                    sorted_data[id].append(data[i])
+            for i in range(len(sorted_data)):
+                sorted_data[i] = sorted(sorted_data[i], key=lambda d: d['Time'])
+            json.dump(sorted_data, sorted_file, indent=4)
+
+
+
+api_key = 'e44d6e67-41b1-4e29-afb5-02ca18deb69c'
+bus_pos_url = 'https://api.um.warszawa.pl/api/action/busestrams_get/?'
+bus_route_url = 'https://api.um.warszawa.pl/api/action/public_transport_routes/?'
+bus_stop_url = 'https://api.um.warszawa.pl/api/action/busestrams_get/?'
+bus_stop_location_url = 'https://api.um.warszawa.pl/api/action/dbstore_get/?ab75c33d-3a26-4342-b36a-6e5fef0a3ac3'
+
+#bus_positions = get_bus_location(bus_pos_url, api_key)
+#bus_positions2 = get_bus_location(bus_pos_url, api_key)
+#bus_route = get_bus_route(bus_route_url, api_key)
+bus_stop_location = get_bus_stop_location(bus_stop_url, api_key)
+
+filename = 'bus_positions.json'
+result_filename = 'bus_positions_clean.json'
+sorted_filename = 'bus_positions_divided.json'
+start_hour = 13
+
+filename2 = 'bus_positions_2.json'
+result_filename2 = 'bus_positions_clean2.json'
+sorted_filename2 = 'bus_positions_divided2.json'
+start_hour2 = 16
+
+bus_timetable_filename = 'bus_timetable.json'
+bus_timetable_clean_filename = 'bus_timetable_clean.json'
+bus_route_filename = 'bus_route.json'
+bus_route_clean_filename = 'bus_route_clean.json'
+bus_stop_location_filename = 'bus_stop_location.json'
+
+#save_to_json(bus_route, bus_route_filename)
+#clean_route_file(bus_route_filename, bus_route_clean_filename)
+
+#save_to_json(bus_stop_location, bus_stop_location_filename)
+
+#save_to_json(bus_positions, filename)
+#save_to_json(bus_positions2, filename2)
+
+#clean_file(filename, result_filename)
+#divide_by_vehicle(result_filename, sorted_filename, start_hour)
+
+#clean_file(filename2, result_filename2)
+#divide_by_vehicle(result_filename2, sorted_filename2, start_hour2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+counter = 0
+with open('test1-1-j.json', 'r') as file1:
+    data = json.load(file1)
+    for i in range(len(data)):
+        if type(data[i]) != dict:
+            #data.pop(i)
+            continue
+        print(data[i]["Lines"])
+        if data[i]["Lines"] == "213":
+            counter += 1
+    print(counter)
+
+    #print(bus_data[0]["result"][0])
+    
+"""
